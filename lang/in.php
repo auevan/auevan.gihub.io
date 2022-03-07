eb6054060a584b28aefa20a8756836871ef5df12
auevan <ahmadaufannh@gmail.com>
2022-03-07 18:51:10 +0700

    Tambah Fitur Bahasa 1

---
+++ lang/in.php
@@ -0,0 +1,21 @@
+<?php 
+	$lang = array(
+		"en" => "Inggris",
+		"in" => "Indonesia",
+		"gelar" => "Mahasiswa Teknik Informatika",
+		"menu1" => "Biodata",
+		"menu2" => "Keterampilan",
+		"menu3" => "Pendidikan",
+		"menu4" => "Kontak",
+		"bio1" => "Halo! Saya Ahmad Aufan Nur Hakim, anda bisa memanggil saya Aufan (alias Auevan).",
+		"bio2" => "Saya lahir di Rembang, Jawa Tengah, Indonesia pada tanggal 2 Oktober 2002. Sekarang saya adalah mahasiswa Jurusan Teknik Informatika di Universitas Muria Kudus. Hobi saya adalah coding. Selain itu, saya juga senang bergabung dengan organisasi untuk meningkatkan kemampuan saya. Saya sering mengikuti kompetisi untuk menguji keterampilan yang telah saya peroleh sepanjang hidup saya.",
+		"info" => "Informasi dasar",
+		"umur" => "Umur",
+		"telpon" => "Telepon",
+		"alamat" => "Alamat",
+		"bahasa" => "Bahasa",
+		"iumur" => "19 tahun",
+		"ialamat" => "Rembang, Jawa Tengah, Indonesia",
+		"ibahasa" => "Indonesia, Inggris, Jawa"
+	);
+?>
